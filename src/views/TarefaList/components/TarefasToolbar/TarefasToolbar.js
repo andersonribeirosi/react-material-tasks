673fd15c791d38a53ef54cfad6c9e0aaf7f1e11b
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-06 01:52:30 -0300

    Iniciando projeto React utilizando Redux, Bootstrap, Material Ui e integração com API Rest

---
+++ src/views/TarefaList/components/TarefasToolbar/TarefasToolbar.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Button,
+  TextField,
+  Grid,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl
+} from '@material-ui/core';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  row: {
+    height: '42px',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: theme.spacing(1)
+  },
+  spacer: {
+    flexGrow: 1
+  },
+  importButton: {
+    marginRight: theme.spacing(1)
+  },
+  exportButton: {
+    marginRight: theme.spacing(1)
+  },
+  searchInput: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const TarefasToolbar = props => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  const [descricao, setDescricao] = useState('')
+  const [categoria, setCategoria] = useState('')
+
+  const submit = (event) => {
+    event.preventDefault()
+    console.log(`Descrição: - ${descricao}, Categoria: - ${categoria}`)
+  }
+
+  return (
+    <div
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <div className={classes.row}>
+        <span className={classes.spacer} />
+
+      </div>
+      <div className={classes.row}>
+        <Grid container>
+          <Grid item md={4}>
+            <TextField
+              className={classes.searchInput}
+              placeholder="Descrição da Tarefa"
+              label="Descrição"
+              value={descricao}
+              onChange={e => setDescricao(e.target.value)}
+              fullWidth
+            />
+          </Grid>
+          <Grid item md={4}>
+            <FormControl fullWidth>
+              <InputLabel>Categoria: </InputLabel>
+              <Select value={categoria} onChange={e => setCategoria(e.target.value)}>
+                {/* <MenuItem value="">Selecione</MenuItem> */}
+                <MenuItem value={"TRABALHO"}>Trabalho</MenuItem>
+                <MenuItem value={"ESTUDOS"}>Estudos</MenuItem>
+                <MenuItem value={"OUTROS"}>Outros</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid>
+            <Button onClick={submit} variant="contained" color="secondary">Adicionar </Button>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+TarefasToolbar.propTypes = {
+  className: PropTypes.string
+};
+
+export default TarefasToolbar;
