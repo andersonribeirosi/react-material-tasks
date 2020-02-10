4b8a929635e449cdb7a97c33e4bfa3514e2fe5ef
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-09 22:54:42 -0300

    Action creator de listagem de tarefas

--- src/store/tarefasReducer.js
+++ src/store/tarefasReducer.js
@@ -1,3 +1,9 @@
+import axios from 'axios'
+
+const http = axios.create({
+    baseURL: 'https://minhastarefas-api.herokuapp.com'
+})
+
 const ACTIONS = {
     lISTAR: 'TAREFAS_LISTA',
     REMOVER: 'TASKS_REMOVE',
@@ -11,9 +17,21 @@ const ESTADO_INICIAL = {
 export const tarefaReducer = (state = ESTADO_INICIAL, action) => {
     switch (action.type) {
         case ACTIONS.lISTAR:
-            return {...state, tarefas: action.tarefas }
+            return { ...state, tarefas: action.tarefas }
         default:
             return state;
     }
+}
 
+export function listar() {
+    return dispatch => {
+        http.get('/tarefas', {
+            headers: { 'x-tenant-id': localStorage.getItem('usuario_logado') }
+        }).then(response => {
+            dispatch({
+                type: ACTIONS.lISTAR,
+                tarefas: response.data
+            })
+        })
+    }
 }
